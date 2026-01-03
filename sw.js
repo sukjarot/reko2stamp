96ee482509352fa417279287f91e8ccef7623d37
sukjarot <jfshark0010@gmail.com>
2026-01-03 22:05:18 +0700

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reko-stamp-v2.5';
+const CACHE_NAME = 'reko-stamp-v2.5.1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -53,6 +53,7 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
 
 
 
